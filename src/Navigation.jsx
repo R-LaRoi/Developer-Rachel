a59072e52d6a662f40941ac2cf6e4c30aec1c119
R. LaRoi <are@raewhoo.com>
2023-05-06 09:36:16 +0200

    Nav

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -7,29 +7,18 @@ export default function Navigation(){
 return(
     <section>
 
-  <div className='nav-bar mobile' >
-    
-     <Link to='/goals'><i className="fa-sharp fa-solid fa-bars"></i></Link>
-    <div className='mobile-links'>
- 
-{/* <input type="checkbox" id="checkbox_toggle" />
-<label for="checkbox_toggle" className="hamburger">&#9776;</label> */}
-
-        <li><Link to='/goals'>goals</Link></li>
-        <li><Link to='/projects'>projects</Link></li>
-        <li><Link to='/contact'  >connect</Link></li>
-      </div>
-
-
-<li><Link to='/goals' className='a-link '  >goals</Link></li>
+  <div className='nav-bar'>
+   
+<li><Link to='/goals' className='a-link ' >goals</Link></li>
 
 <li><Link to='/projects' className='a-link'>projects</Link></li>
 
 <li><Link to='/contact' className='a-link' >connect</Link></li>
 <li><Link to='/home' className='a-link initial' >RS</Link></li>
-
+<li><Link to='/home' className='a-link home' ><i class="fa-solid fa-house"></i></Link></li>
 
 </div>
+
   </section>
 
 
