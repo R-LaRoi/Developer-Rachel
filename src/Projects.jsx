211b0c0be25047a686e1ae4cae13fb19064bfac6
R. LaRoi <are@raewhoo.com>
2023-05-07 17:56:36 +0200

    Icons

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -25,11 +25,15 @@ export default function Projects() {
        <div >PROJECTS</div>
   </div>
 
-<img src={image1} className='project-images' alt="no alternative text" ></img>
+<img src="https://user-images.githubusercontent.com/114012059/236687965-9f7e25ce-f10f-40d4-8264-c4f79ff43def.png" alt="" srcset="" className='project-images' />
     
 {/* ---------- */}
 <div className='img-grid'>
-<img src={image2} className='project-images' alt="no alternative tex" ></img>
+
+  <img src="https://user-images.githubusercontent.com/114012059/236687972-62d609d9-64b7-422a-9775-499848b4165e.png" alt="no alternative text" className='project-images' />
+{/* <img src={image2} className='project-images' alt="no alternative tex" ></img> */}
+
+
 <div className='text'> <ul>
   <li>Whats the move? </li>
   <li>Fitness Coach Profile </li>
@@ -42,7 +46,8 @@ export default function Projects() {
 
 
 <div className='img-grid'>
-<img src={image2} className='project-images'  alt="no alternative tex"></img>
+   <img src="https://user-images.githubusercontent.com/114012059/236687956-50ff372e-8003-471f-b697-6a13fed89ab2.png" alt="" className='project-images' />
+{/* <img src={image2} className='project-images'  alt="no alternative tex"></img> */}
 <div className='text'><ul>
   <li>Whats the move? </li>
   <li>Fitness Coach Profile </li>
@@ -52,12 +57,17 @@ export default function Projects() {
 </div>
 {/* ---------- */}
 <div className='img-grid'>
-<img src={image1} className='project-images'  alt="no alternative tex"></img>
+  <img src="https://user-images.githubusercontent.com/114012059/236687960-232d5984-6f53-4634-9eb8-61993337e35c.png" 
+  
+  
+  className='project-images' />
+{/* <img src={image1} className='project-images'  alt="no alternative tex"></img> */}
 <div className='text'> project one </div>
 </div>
 
 <div className='img-grid'>
-<img src={image1} className='project-images'  alt="no alternative tex"></img>
+  <img src="https://user-images.githubusercontent.com/114012059/236687956-50ff372e-8003-471f-b697-6a13fed89ab2.png" alt="" className='project-images' />
+{/* <img src={image1} className='project-images'  alt="no alternative tex"></img> */}
 <div className='text-ovr'> visit </div>
 <div className='text'> project two</div>
 
