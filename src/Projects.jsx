7aef98a7e1b2d7a734b58fb0280e63b77c50c3a9
R. LaRoi <are@raewhoo.com>
2023-05-13 23:23:22 +0200

    Project Images

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./projects.css"
 import Navigation from './Navigation'
 import Footer from './footer'
+import busybot from "./busybot.png"
+import image2 from "./image2.png"
 
 export default function Projects() {
 
@@ -20,7 +22,9 @@ export default function Projects() {
 
 </div>
 
+<div className='w-1'>  <img src={busybot} alt="" className='project-images' />
 
+</div>
 
 <div className='b-1'><img src="https://user-images.githubusercontent.com/114012059/236687972-62d609d9-64b7-422a-9775-499848b4165e.png" alt="no alternative text" className='project-images' />
 
@@ -36,8 +40,9 @@ export default function Projects() {
   
 </div></div>
 
-<div className='w-1'>  <img src="https://user-images.githubusercontent.com/114012059/236687956-50ff372e-8003-471f-b697-6a13fed89ab2.png" alt="" className='project-images' /></div>
-<div className='b-1'><img src="https://user-images.githubusercontent.com/114012059/236687972-62d609d9-64b7-422a-9775-499848b4165e.png" alt="no alternative text" className='project-images' /></div>
+
+
+<div className='b-1'><img src={image2} alt="no alternative text" className='project-images' /></div>
 
 <div className='b-1'> 
 <img src="https://user-images.githubusercontent.com/114012059/236687965-9f7e25ce-f10f-40d4-8264-c4f79ff43def.png" alt="" srcset="" className='project-images' />
